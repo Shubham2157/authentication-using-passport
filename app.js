a16f101d5fa3dab52a0b544f2ed826f47f4db4bc
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2021-01-10 21:55:55 +0530

    added middleware for isLoggedIn

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get("/", (req,res) => {
     res.render("home")
 });
 
-app.get("/secret", (req,res) => {
+app.get("/secret",isLoggedIn ,(req,res) => {
     res.render("secret")
 });
 
@@ -78,6 +78,15 @@ app.get("/logout", (req,res) => {
     req.logOut();
     res.redirect("/");
 })
+
+// middleware
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect("/login");
+}
 
 const PORT = 3500
 app.listen(PORT, ()=>{
