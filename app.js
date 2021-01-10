4eb257c9ddab06e7b29d0010bf0153341d954451
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2021-01-10 18:29:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express   = require("express"),
+      mongoose  = require('mongoose')
+
+
+      mongoose.connect("mongodb://localhost/auth_demo_app")
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get("/", (req,res) => {
+    res.render("home")
+})
+
+app.get("/secret", (req,res) => {
+    res.render("secret")
+})
+
+const PORT = 3500
+app.listen(PORT, ()=>{
+    console.log(`Server started at ${PORT}`);
+})
