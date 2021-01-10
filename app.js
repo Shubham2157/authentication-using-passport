b621f74c1f1a58d8d9396ec2ed6cf828a791a578
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2021-01-10 20:44:22 +0530

    setup passport in app.js

--- app.js
+++ app.js
@@ -7,10 +7,21 @@ const express               = require("express"),
       passportLocalMongoose = require('passport-local-mongoose')
 
 
-      mongoose.connect("mongodb://localhost/auth_demo_app")
+mongoose.connect("mongodb://localhost/auth_demo_app")
 
 const app = express();
 app.set('view engine', 'ejs');
+
+app.use(require('express-session')({
+    secret: "Mai hun Don",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req,res) => {
     res.render("home")
